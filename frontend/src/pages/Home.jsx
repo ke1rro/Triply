8a8eb06003c1a6b561a064b31730c63bb52b6915
Andriy <kryvyi.andriy@gmail.com>
2025-07-16 11:59:39 +0300

    Format

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -110,7 +110,7 @@ const Homepage = () => {
     <div className="relative flex min-h-screen flex-col">
       {/* Background image with overlay - full screen */}
       <div
-        className="fixed inset-0 bg-cover bg-center bg-no-repeat h-lvh"
+        className="fixed inset-0 h-lvh bg-cover bg-center bg-no-repeat"
         style={{
           backgroundImage: `url('https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')`,
         }}
