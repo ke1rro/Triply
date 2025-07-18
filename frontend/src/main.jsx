fb91079e8b5e450086a399cf136bfa62ae3c73e7
Andriy <kryvyi.andriy@gmail.com>
2025-07-18 10:15:58 +0300

    Fix routing in react

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -23,7 +23,7 @@ import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
